e6a02a6b80c0c593914136859bb3f862675a80f0
ThiagoVRibeiro <thvinicius1@gmail.com>
2026-02-23 11:03:24 -0300

    feat: feito relatorio de servicos realizados

--- src/main/java/com/study/haircut2/repository/ServicoRepository.java
+++ src/main/java/com/study/haircut2/repository/ServicoRepository.java
@@ -1,9 +1,13 @@
 package com.study.haircut2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.haircut2.model.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
 
+	//Adicionado para gerar relatório de serviços realizados
+	List<Servico> findAllByOrderByDataDesc();
 }
